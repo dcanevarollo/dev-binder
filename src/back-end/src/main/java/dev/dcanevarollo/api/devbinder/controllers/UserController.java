b9d1969f16e9ec1c4603c66048b6fc0c8700a8d0
Douglas Canevarollo <douglascanevarollo@gmail.com>
2020-08-10 20:46:52 -0300

    Structuring error handling in the controller

--- src/back-end/src/main/java/dev/dcanevarollo/api/devbinder/controllers/UserController.java
+++ src/back-end/src/main/java/dev/dcanevarollo/api/devbinder/controllers/UserController.java
@@ -1,10 +1,11 @@
 package dev.dcanevarollo.api.devbinder.controllers;
 
 import dev.dcanevarollo.api.devbinder.models.User;
-import dev.dcanevarollo.api.devbinder.services.UserService;
+import dev.dcanevarollo.api.devbinder.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -13,22 +14,18 @@ import java.util.UUID;
 public class UserController {
 
     @Autowired
-    UserService service;
+    UserRepository repository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User store(@RequestBody User user) {
-        return service.save(user);
+        return repository.save(user);
     }
 
     @GetMapping("/{id}")
     public User show(@PathVariable UUID id) {
-        try {
-            return service.findById(id);
-        } catch (Exception err) {
-            err.printStackTrace();
-            return null;
-        }
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
 }
--- src/back-end/src/main/java/dev/dcanevarollo/api/devbinder/services/UserService.java
+++ src/back-end/src/main/java/dev/dcanevarollo/api/devbinder/services/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -22,18 +21,6 @@ public class UserService implements UserDetailsService {
         Optional<User> user = repository.findByUsername(username);
 
         if (!user.isPresent()) throw new UsernameNotFoundException(username);
-
-        return user.get();
-    }
-
-    public User save(User user) {
-        return repository.save(user);
-    }
-
-    public User findById(UUID id) throws Exception {
-        Optional<User> user = repository.findById(id);
-
-        if (!user.isPresent()) throw new Exception();
 
         return user.get();
     }
