62ab00c0196acd47b81848dd26cf400ad98a872f
Douglas Canevarollo <douglascanevarollo@gmail.com>
2020-08-18 16:29:47 -0300

    Adapting User interface

--- src/front-end/src/app/shared/models/tech.model.ts
+++ src/front-end/src/app/shared/models/tech.model.ts
@@ -1,7 +1,4 @@
-import { User } from './user.model';
-
 export interface Tech {
   id: string;
   name: string;
-  users: User[];
 }
--- src/front-end/src/app/shared/models/user.model.ts
+++ src/front-end/src/app/shared/models/user.model.ts
@@ -2,16 +2,16 @@ import { Tech } from './tech.model';
 import { Post } from './post.model';
 
 export interface User {
-  id?: string;
-  name?: string;
+  id: string;
+  name: string;
   username: string;
   password: string;
   avatar_url?: string;
   current_job?: string;
   bio?: string;
-  github_url?: string;
   posts?: Post[];
-  followers?: User[];
-  liked_posts?: Post[];
   techs?: Tech[];
+  github_url: string;
+  followers_count?: number;
+  following_count?: number;
 }
